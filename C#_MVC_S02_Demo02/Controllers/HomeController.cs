f5d8a12ddf5baf1caf4f6881ff012d285d20ebc6
Hazem Fady <hazemfady123@gmail.com>
2024-09-04 17:28:18 +0300

    Create All Views Project

---
+++ C#_MVC_S02_Demo02/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C__MVC_S02_Demo02.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
--- C#_MVC_S02_Demo02/Program.cs
+++ C#_MVC_S02_Demo02/Program.cs
@@ -10,22 +10,26 @@ namespace C__MVC_S02_Demo02
             //builder.Services.AddControllers();//To Registers Web APIS Services
             //builder.Services.AddControllersWithViews();//To Registers MVC Services
             //builder.Services.AddRazorPages();//To Registers Razors Pages Services
-            //builder.Services.AddMvc();//To Registers all web [MVC,APIS,Razor Pages]  Services
+            builder.Services.AddMvc();//To Registers all web [MVC,APIS,Razor Pages]  Services
+           
             var app = builder.Build();
+
+            app.UseStaticFiles();   //to register Middle ware
             ////Routing
             //app.MapGet("/", () => "Hello World!");
             //app.MapGet("/hamada", () => "Hello Hamada!");
 
 
             ////To Make Routing Based ON Controller  (Movies)
-            //app.MapControllerRoute
-            //    (
-            //    name: "Default",
-            //    //pattern: "/controller/action/id"//static
-            //   pattern: "/{controller}/{action}/{id}"//static
-               //pattern: "/{controller=Movies}/{action=GetMovies}/{id=1}"//Constraint 
-               // pattern: "/{controller=Movies}/{action=GetMovies}/{id:int?}"//Constraint 
-               // );
+            app.MapControllerRoute
+                (
+                name: "Default",
+               //pattern: "/controller/action/id"//static
+              // pattern: "/{controller}/{action}/{id}"//static
+               pattern: "/{controller}/{action}"//static
+                                                     //pattern: "/{controller=Movies}/{action=GetMovies}/{id=1}"//Constraint 
+                                                     // pattern: "/{controller=Movies}/{action=GetMovies}/{id:int?}"//Constraint 
+                );
 
             app.Run();
         }
