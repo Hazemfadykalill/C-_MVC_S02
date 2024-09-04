049307f24d423f54e892462c6802c691e15e9958
Hazem Fady <hazemfady123@gmail.com>
2024-09-04 12:20:52 +0300

    Routing V01

---
+++ C#_MVC_S02_Demo02/Controllers/MoviesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace C__MVC_S02_Demo02.Controllers
+{
+    public class MoviesController:Controller
+    {
+
+        //Action
+        public string GetMovies(int Id)
+        {
+            return $"Id Is : {Id}";
+        }
+    }
+}
--- C#_MVC_S02_Demo02/Program.cs
+++ C#_MVC_S02_Demo02/Program.cs
@@ -5,9 +5,25 @@ namespace C__MVC_S02_Demo02
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
 
+            //To Add Any Services Should be before build
+            builder.Services.AddControllers();//To Registers Web APIS Services
+            builder.Services.AddControllersWithViews();//To Registers MVC Services
+            builder.Services.AddRazorPages();//To Registers Razors Pages Services
+            builder.Services.AddMvc();//To Registers all web [MVC,APIS,Razor Pages]  Services
+            var app = builder.Build();
+            //Routing
             app.MapGet("/", () => "Hello World!");
+            app.MapGet("/hamada", () => "Hello Hamada!");
+
+
+            //To Make Routing Based ON Controller  (Movies)
+            app.MapControllerRoute
+                (
+                name: "Default",
+               // pattern: "/controller/action/id"//static
+                pattern: "/{controller}/{action}/{id}"//static
+                );
 
             app.Run();
         }
